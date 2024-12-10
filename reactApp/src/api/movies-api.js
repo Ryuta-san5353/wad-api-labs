ea3a2483d2a4858f579ee97373b2e3695d69415b
Ryuta-san5353 <20110781@mail.wit.ie>
2024-12-11 08:37:30 +0900

    Integration Lab - React app setup

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,4 +1,4 @@
-export const getMovies = async () => {
+export const getMovies = async ()=> {
     const response = await  fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=819ced827ccb486b293f66d1062c837b&language=en-US&include_adult=false&page=1`
     )
